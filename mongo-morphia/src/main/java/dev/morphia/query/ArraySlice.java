1eb785405d7d31734638b64461fb90211b24751f
Mike Hummel <>
2019-09-16 22:53:55 +0200

    1.6.2

--- mongo-morphia/src/main/java/dev/morphia/query/ArraySlice.java
+++ mongo-morphia/src/main/java/dev/morphia/query/ArraySlice.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2008-2016 MongoDB, Inc.
+/**
+ * Copyright (c) 2008-2015 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package dev.morphia.query;
 
 import java.util.Arrays;
